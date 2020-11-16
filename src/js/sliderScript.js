62de4e66dd0b06ece961974ff9abe90daa8d4717
Stephen Igwebuike <staicciniventures@gmail.com>
2020-11-16 10:15:28 +0100

    created the menu slider using slick.js

---
+++ src/js/sliderScript.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+  $('.sliderSection').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    adaptiveHeight: true,
+  });
+});
